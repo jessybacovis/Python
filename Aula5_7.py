059e52513f4f16109df97ace7490da5248360f5f
Jessybacovis <64629113+jessybacovis@users.noreply.github.com>
2023-03-12 01:01:29 -0300

    Update Aula5_7.py

--- Aula5_7.py
+++ Aula5_7.py
@@ -1,15 +1,1 @@
-# Python program to show pyplot module
-import matplotlib.pyplot as plt
-import pandas as pd
 
-df = pd.read_csv("Iris.csv")
-
-# This will plot a simple bar chart
-plt.bar(df['Species'], df['SepalLengthCm'])
-
-# Title to the plot
-plt.title("Iris Dataset")
-
-# Adding the legends
-plt.legend(["bar"])
-plt.show()
