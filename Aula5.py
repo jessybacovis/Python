dcb88e6c957d50c450f5cffea6f82cdeed0b6593
Jessybacovis <64629113+jessybacovis@users.noreply.github.com>
2023-03-12 00:35:12 -0300

    Add files via upload

---
+++ Aula5.py
@@ -0,0 +1,10 @@
+import numpy as numpy
+
+b = numpy.empty(2, dtype = int)
+print("Matrix b : \n", b)
+
+a = numpy.empty([2, 2], dtype = int)
+print("\nMatrix a : \n", a)
+
+c = numpy.empty([3, 3])
+print("\nMatrix c : \n", c)
