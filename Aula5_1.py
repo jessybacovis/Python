86e7d25f6ca590f9c0b6125349b96ee671b4d39b
Jessybacovis <64629113+jessybacovis@users.noreply.github.com>
2023-03-12 00:41:29 -0300

    Add files via upload

---
+++ Aula5_1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+b = np.zeros(2, dtype = int)
+print("Matrix b : \n", b)
+
+a = np.zeros([2, 2], dtype = int)
+print("\nMatrix a : \n", a)
+
+c = np.zeros([3, 3])
+print("\nMatrix c : \n", c)
